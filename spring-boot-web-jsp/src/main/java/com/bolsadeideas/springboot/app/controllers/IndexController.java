6b5ddc571d985c4334b7c7a4ef21bfa936f17565
Nicolas Gatti <gatti2602@gmail.com>
2019-01-05 00:05:49 -0300

    init

---
+++ spring-boot-web-jsp/src/main/java/com/bolsadeideas/springboot/app/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package com.bolsadeideas.springboot.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Value;
+
+@Controller
+public class IndexController {
+	
+	@Value("${application.controller.titulo}")
+	private String titulo;
+	
+	@GetMapping("/index")
+	public String index(Model model) {
+		model.addAttribute("titulo", titulo);
+		return "index";	//Nombre de la vista
+	}
+}
